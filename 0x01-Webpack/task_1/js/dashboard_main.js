b8668f962882c0b38fe0bbfbcb8b2badf2cd19e6
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2022-09-29 14:59:38 +0100

    all

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,4 +1,5 @@
-const $ = require('jquery');
+import $ from 'jquery';
+const _ = require('lodash');
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
