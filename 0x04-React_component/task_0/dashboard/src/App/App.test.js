796761260de998c2bc34bb4d460949835d635a80
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2022-10-24 13:27:11 +0100

    component

---
+++ 0x04-React_component/task_0/dashboard/src/App/App.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Enzyme from 'enzyme';
+import { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from './App.js';
+
+Enzyme.configure({adapter: new Adapter()})
+
+
+let wrapper;
+
+beforeEach(() => {
+  wrapper = shallow(<App />);
+});
+
+describe('testing component <App />', () => {
+  it('render without crashing', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders three list items', () => {
+    expect(wrapper.find('div.App')).toHaveLength(1);
+  })
+it ('should render a div with a className "Notifications"', () =>{
+  expect(wrapper.find("Notifications")).toHaveLength(1);
+})
+it ('should render a div with a className "App-header"', () => {
+  expect(wrapper.find("Header")).toHaveLength(1);
+})
+it ('should render a div with a className "App-body"', () => {
+  expect(wrapper.find("Login")).toHaveLength(1);
+})
+it ('should render a div with a className "App-footer"', () => {
+  expect(wrapper.find("Footer")).toHaveLength(1);
+})
+});
